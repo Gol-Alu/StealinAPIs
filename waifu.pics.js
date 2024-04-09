6c7c6ef7772de8d73bef7f50bf41070cbe8af357
towsifkafi <kafitowsif@gmail.com>
2024-04-10 01:00:48 +0600

    🔪 waifu.pics hehe

---
+++ waifu.pics.js
@@ -0,0 +1,84 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const reactions = [
+    "bully",
+    "cuddle",
+    "cry",
+    "hug",
+    "awoo",
+    "kiss",
+    "lick",
+    "pat",
+    "smug",
+    "bonk",
+    "yeet",
+    "blush",
+    "smile",
+    "wave",
+    "highfive",
+    "handhold",
+    "nom",
+    "bite",
+    "glomp",
+    "slap",
+    "kill",
+    "kick",
+    "happy",
+    "wink",
+    "poke",
+    "dance",
+    "cringe"
+];
+
+async function downloadGif(reaction) {
+    const apiUrl = `https://api.waifu.pics/many/sfw/${reaction}`;
+    try {
+        const response = await axios.post(apiUrl, {});
+        const gifUrl = response.data.files;
+        //console.log(gifUrl)
+        const folderPath = path.join(__dirname, 'waifu.pics', reaction);
+
+        gifUrl.forEach(async f => {
+            const gifName = f.split('/').pop(); // Extracting the name from the URL
+            const filePath = path.join(folderPath, gifName);
+
+            if (!fs.existsSync(filePath)) {
+                if (!fs.existsSync(folderPath)) {
+                    fs.mkdirSync(folderPath, { recursive: true });
+                }
+
+                try {
+                    const gifResponse = await axios.get(f, { responseType: 'arraybuffer' });
+                    fs.writeFileSync(filePath, gifResponse.data);
+                } catch(err) {
+                    console.log("[ERROR] "+ err.message)
+                }
+                
+                console.log(`[${reaction}] Downloaded ${filePath}`);
+            } else {
+                console.log(`[${reaction}] File ${f} already exists. Skipping...`);
+            }
+            await new Promise(resolve => setTimeout(resolve, 100));
+        })
+
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function downloadGifs() {
+    let init = 0;
+    for (let i = 0; i < 10000; i++) {
+        if(init >= reactions.length) init = 0;
+
+        const reaction = reactions[init];
+        await downloadGif(reaction);
+        await new Promise(resolve => setTimeout(resolve, 100)); // 1-second delay
+        init = init+1
+    }
+}
+
+downloadGifs();
