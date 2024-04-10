d14af03603ab0f2bb4da2bf11f605decdeb8e0fc
towsifkafi <kafitowsif@gmail.com>
2024-04-10 13:45:45 +0600

    compressssorrr go brrr

---
+++ optimize.js
@@ -0,0 +1,19 @@
+const compress_images = require("compress-images");
+let INPUT_PATH, OUTPUT_PATH;
+
+INPUT_PATH = "ALL/*/*.gif";
+OUTPUT_PATH = "OPTIMIZED/";
+
+compress_images(INPUT_PATH, OUTPUT_PATH, { compress_force: false, statistic: true, autoupdate: true }, false,
+                { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+                { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+                { svg: { engine: "svgo", command: "--multipass" } },
+                { gif: { engine: "gifsicle", command: ["-O", "--lossy=80"] } },
+  function (error, completed, statistic) {
+    console.log("-------------");
+    console.log(error);
+    console.log(completed);
+    console.log(statistic);
+    console.log("-------------");
+  }
+);
